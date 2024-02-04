92a65589d10d2ae29ea78366ca98b4563ada6a85
Lior Dolev <liordo@liordo-mbp.local>
2024-02-04 12:15:38 +0200

    add files accordion story example

--- src/Accordion/Accordion.stories.tsx
+++ src/Accordion/Accordion.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta } from '@storybook/react';
 import { Accordion, AccordionDetails, Typography } from '@mui/material';
 import AccordionSummary from './AccordionSummary';
-import { Tooltip } from '..';
+import { FileViewer, SourceDateStepSubLabel, Stepper, Tooltip } from '..';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -13,19 +13,76 @@ const meta = {
 
 export default meta;
 
-const text =
-  'פירוט המרכיבים ועוד הרבה דברים הרבה יותר ממה שיכול להיכנס באורך הזה';
-
 export const Default = () => (
-  <Accordion>
-    <AccordionSummary aria-controls="panel2-content" id="panel2-header">
-      <Tooltip title={text}>
-        <Typography>{text}</Typography>
+  <Accordion defaultExpanded>
+    <AccordionSummary>
+      <Tooltip title={'כותרת'}>
+        <Typography>{'כותרת'}</Typography>
       </Tooltip>
     </AccordionSummary>
     <AccordionDetails>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      malesuada lacus ex, sit amet blandit leo lobortis eget.
+      <Typography>
+        לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית הועניב היושבב שערש
+        שמחויט - שלושע ותלברו חשלו שעותלשך וחאית נובש ערששף. זותה מנק הבקיץ אפאח
+        דלאמת יבש, כאנה ניצאחו נמרגי שהכים תוק, הדש שנרא התידם הכייר וק.
+      </Typography>
     </AccordionDetails>
   </Accordion>
 );
+
+export const TitleTextOverflow = () => (
+  <Accordion>
+    <AccordionSummary>
+      <Tooltip
+        title={
+          'פירוט המרכיבים ועוד הרבה דברים הרבה יותר ממה שיכול להיכנס באורך הזה'
+        }
+      >
+        <Typography>
+          {
+            'פירוט המרכיבים ועוד הרבה דברים הרבה יותר ממה שיכול להיכנס באורך הזה'
+          }
+        </Typography>
+      </Tooltip>
+    </AccordionSummary>
+    <AccordionDetails>
+      <Typography>
+        לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית הועניב היושבב שערש
+        שמחויט - שלושע ותלברו חשלו שעותלשך וחאית נובש ערששף. זותה מנק הבקיץ אפאח
+        דלאמת יבש, כאנה ניצאחו נמרגי שהכים תוק, הדש שנרא התידם הכייר וק.
+      </Typography>
+    </AccordionDetails>
+  </Accordion>
+);
+
+/**
+ *
+ * @todo - Add invalid file indication
+ */
+export const FilesAccordion = () => {
+  const steps = [
+    {
+      id: 'landscape',
+      label: 'landscape.pdf',
+      content: <FileViewer src={'landscape.pdf'} height={300} />,
+      subLabel: <SourceDateStepSubLabel source={'סתם'} time={1700642439921} />,
+    },
+    {
+      id: 'hebrew',
+      label: 'האוניברסיטה העברית.pdf',
+      content: <FileViewer src={'עברית.pdf'} height={300} />,
+      subLabel: (
+        <SourceDateStepSubLabel source={'משרד החינוך'} time={1700642479921} />
+      ),
+    },
+  ];
+
+  return (
+    <Accordion>
+      <AccordionSummary>{`קבצים (${steps.length})`}</AccordionSummary>
+      <AccordionDetails>
+        <Stepper defaultActiveStepIds={['hebrew']} steps={steps} />
+      </AccordionDetails>
+    </Accordion>
+  );
+};
