220e828aff13047fe04a6947e081313174562aab
Gabriel Sapojnikov <gabisap1812@gmail.com>
2025-01-13 13:14:59 +0200

    fix the height

--- packages/search/src/SearchResultsDropdown.tsx
+++ packages/search/src/SearchResultsDropdown.tsx
@@ -23,7 +23,7 @@ export interface ISearchResultsDropdownProps {
 
 const dropdownCss = css({
   width: '500px',
-  height: '20vh',
+  // height: '20vh',
   overflowY: 'auto',
   margin: 0,
   padding: '2px',
